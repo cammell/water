c684432b1c59ef60af51d427489fdbdf73f70abd
Cammell <cammell@gmail.com>
2021-05-17 22:36:48 +0200

    added js data prototype - not finished yet

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,27 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+
+
+const entry={
+    current: new Date().toISOString().slice(0, 10),
+    glasses: 0,
+}
+
+let data = [];
+
+//check if today exist in dataset and if not, create it.
+if(data) {
+    data.push(entry);
+    console.log(data[0]);
+}
+
+const add=document.querySelector(".form__image");
+const remove=document.querySelector(".form__button");
+
+add.addEventListener('click', event => {
+
+
+
+})
